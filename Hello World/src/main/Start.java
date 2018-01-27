6fda41fece992b984cb2db8604f0a7a232f6fec3
Admini <Admini@DESKTOP-56DCT8C>
2018-01-27 03:38:41 +0200

    new commit

--- Hello World/src/main/Start.java
+++ Hello World/src/main/Start.java
@@ -3,6 +3,6 @@ package main;
 public class Start {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Commit 2");
+		System.out.println("Hi world");
 	}
 }
